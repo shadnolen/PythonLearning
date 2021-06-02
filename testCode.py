31052b8c6af1af99e2dd24b650ee84e781f172d9
Shad Nolen <shadnolen@gmail.com>
2021-06-02 01:11:30 -0500

    working on more tk

--- testCode.py
+++ testCode.py
@@ -1,11 +1,13 @@
 import tkinter as tk
+from tkinter import ttk
+from tkinter.messagebox import showinfo
 
 
 root = tk.Tk()
 root.title('Tkinter Window Demo')
 root.geometry('600x400+50+50')
 root.resizable(False, False)
-root.attributes('-alpha', 0.5)
+root.attributes('-alpha', 0.9)
 
 window_width = 300
 window_height = 200
@@ -21,5 +23,45 @@ center_y = int(screen_height/2 - window_height / 2)
 
 # set the position of the window to the center of the screen
 root.geometry(f'{window_width}x{window_height}+{center_x}+{center_y}')
+
+
+# store email address and password
+email = tk.StringVar()
+password = tk.StringVar()
+
+
+def login_clicked():
+    """ callback when the login button clicked
+    """
+    msg = f'You entered email: {email.get()} and password: {password.get()}'
+    showinfo(
+        title='Information',
+        message=msg
+    )
+
+
+# Sign in frame
+signin = ttk.Frame(root)
+signin.pack(padx=10, pady=10, fill='x', expand=True)
+
+
+# email
+email_label = ttk.Label(signin, text="Email Address:")
+email_label.pack(fill='x', expand=True)
+
+email_entry = ttk.Entry(signin, textvariable=email)
+email_entry.pack(fill='x', expand=True)
+email_entry.focus()
+
+# password
+password_label = ttk.Label(signin, text="Password:")
+password_label.pack(fill='x', expand=True)
+
+password_entry = ttk.Entry(signin, textvariable=password, show="*")
+password_entry.pack(fill='x', expand=True)
+
+# login button
+login_button = ttk.Button(signin, text="Login", command=login_clicked)
+login_button.pack(fill='x', expand=True, pady=10)
 
 root.mainloop()
